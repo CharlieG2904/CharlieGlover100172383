2b58d91b2cbd4a4c7bba7dd70a1e76f9ee41f74b
CharlieG2904 <100172383@student.burnley.ac.uk>
2023-10-23 12:27:03 +0100

    init
    Defines the data types for the title and artist

--- CG1/src/cg1/MyApp.java
+++ CG1/src/cg1/MyApp.java
@@ -6,3 +6,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
+
+//This section of the code defines the data types for the title & artist (strings) and the number of plays (integer)
+class Song {
+    private String title;
+    private String artist;
+    private int plays;
